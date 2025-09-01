842e2344fa0fed401ccaba791cd047ce82c78053
Brady Adger <brady.adger12@gmail.com>
2025-09-01 11:30:00 -0600

    make notification text smaller

--- app/components/dialogs/GameNotificationDialog.tsx
+++ app/components/dialogs/GameNotificationDialog.tsx
@@ -15,7 +15,7 @@ export const GameNotificationDialog = ({ notifications, game_id }: { notificatio
                     <ul className="menu bg-base-200 w-full rounded-box">
                         {notifications.map((notification, index) => (
                             <li key={index}>
-                                <div className="flex items-center justify-between w-full gap-y-2">
+                                <div className="flex items-center justify-between w-full gap-y-2 text-xs">
                                     <span className="text-base-content">Day {notification.day}</span>
                                     <span className="text-base-content">{notification.description}</span>
                                     <ResourceDisplay resource={notification.resource as Resource} value={notification.resource_change_as_percent} />
