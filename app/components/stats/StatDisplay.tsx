b5446a92ee518c2e07eb4b1217f72826650a0deb
Brady Adger <brady.adger12@gmail.com>
2025-09-12 14:48:48 -0600

    increase text size

--- app/components/stats/StatDisplay.tsx
+++ app/components/stats/StatDisplay.tsx
@@ -3,7 +3,7 @@ export default function StatDisplay({ title, value, description }: { title: stri
         <div className="stats shadow">
             <div className="stat">
                 <div className="stat-title">{title}</div>
-                <div className="stat-value text-sm md:text-2xl lg:text-4xl">{value}</div>
+                <div className="stat-value text-2xl lg:text-4xl">{value}</div>
                 {description && <div className="stat-desc">{description}</div>}
             </div>
         </div>
