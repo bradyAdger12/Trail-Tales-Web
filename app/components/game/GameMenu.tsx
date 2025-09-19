f681df345a23cbaca074ca9737930509ee792678
Brady Adger <brady.adger12@gmail.com>
2025-09-19 15:04:52 -0600

    fix bug

--- app/components/game/GameMenu.tsx
+++ app/components/game/GameMenu.tsx
@@ -21,7 +21,7 @@ export default function GameMenu({ game }: { game: Game }) {
                     <div className="flex-1 flex flex-wrap gap-4 w-full">
                         {game.survival_days.map((item, i) => (
                             <div key={item.id} className={`relative w-22 h-22 rounded-lg border-2 ${i < game.survival_days.length - 1 ? 'border-gray-600' : 'border-primary'}  `}>
-                                {i === game.survival_days.length - 1 && <img src={BrushedX} alt="brushed x" className="absolute top-0 right-0 w-full h-full opacity-50 z-10 pointer-events-none" />}
+                                {i < game.survival_days.length - 1 && <img src={BrushedX} alt="brushed x" className="absolute top-0 right-0 w-full h-full opacity-50 z-10 pointer-events-none" />}
                                 <Link to={`/game/${item.game_id}/survival_day/${item.id}`}>
                                     <div key={item.id} className={`w-full h-full relative rounded-lg bg-black/10 flex flex-col items-center justify-center`}>
                                         <p className="font-bold text-md">{item.day}</p>
