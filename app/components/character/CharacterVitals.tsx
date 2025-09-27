ed303c06fa31a832833c1f20f184805deb938f69
Brady Adger <brady.adger12@gmail.com>
2025-09-27 11:27:44 -0600

    minor layout adjustment for vitals

--- app/components/character/CharacterVitals.tsx
+++ app/components/character/CharacterVitals.tsx
@@ -17,23 +17,29 @@ export default function CharacterVitals({ character }: { character: Character })
                 {user?.display_name} Vitals
             </h4>
             <div className="flex flex-col gap-y-6 mt-4">
-            <div className="flex items-center gap-2">
+                <div className="flex items-center gap-2">
                     <div className="flex-1">
                         <CharacterStat stat={character.health} color={HEALTH_COLOR} icon="fa-heart" />
                     </div>
-                    {totalLoss > 0 && <div className="tooltip" data-tip={lossHint}><StatLoss stat={character.health} loss={totalLoss} /></div>}
+                    <div className="tooltip min-w-19" data-tip={lossHint}>
+                        {totalLoss > 0 && <StatLoss stat={character.health} loss={totalLoss} />}
+                    </div>
                 </div>
                 <div className="flex items-center gap-2">
                     <div className="flex-1">
                         <CharacterStat stat={character.food} color={FOOD_COLOR} icon="fa-utensils" />
                     </div>
-                    <StatLoss stat={character.food} loss={game?.daily_food_loss || 0} />
+                    <div className="min-w-16">
+                        <StatLoss stat={character.food} loss={game?.daily_food_loss || 0} />
+                    </div>
                 </div>
                 <div className="flex items-center gap-x-2">
                     <div className="flex-1">
                         <CharacterStat stat={character.water} color={WATER_COLOR} icon="fa-water" />
                     </div>
-                    <StatLoss stat={character.water} loss={game?.daily_water_loss || 0} />
+                    <div className="min-w-16">
+                        <StatLoss stat={character.water} loss={game?.daily_water_loss || 0} />
+                    </div>
                 </div>
             </div>
         </div>
